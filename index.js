4bcb9beab0fae872a260da8ebde85d811a232280
Rafiul29 <rafiulislam4472@gmail.com>
2021-11-26 12:23:43 -0700

    updates

--- index.js
+++ index.js
@@ -4,17 +4,28 @@ const http=require('http');
 
 const expressServer = http.createServer(app);
 
-const {Server, Socket}=require('socket.io');
+const {Server}=require('socket.io');
 const io=new Server(expressServer);
 
 io.on('connection',function(Socket){
     console.log("New User Connected");
+   
     // Socket.on('disconnect',function(){
     //     console.log("User Disconnected");
     // })
-    setTimeout(function(){
-        Socket.send("Server side - Client side")
-    },10000)
+
+    // setTimeout(function(){
+    //     Socket.send("Server side - Client side")
+    // },10000)
+
+
+    setInterval(() => {
+        let d=new Date();
+        let t =d.getTime();
+       // Socket.send(t);
+        //custom event ..........
+        Socket.emit("myevent",t)
+    },10);
 })
 
 app.get('/',function(req,res){
