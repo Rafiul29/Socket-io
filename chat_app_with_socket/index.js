58d030195ba8f3321c3fddda4409f96046cb8fc8
Rafiul29 <rafiulislam4472@gmail.com>
2021-11-30 13:00:15 +0600

    updates

--- chat_app_with_socket/index.js
+++ chat_app_with_socket/index.js
@@ -1,20 +1,19 @@
-const express=require('express')
-const app=express();
-const http=require('http');
-const expressServer=http.createServer(app);
+const express = require('express')
+const app = express();
+const http = require('http');
+const expressServer = http.createServer(app);
 
+const { Server } = require('Socket.io')
+let io = new Server(expressServer);
 
-
-const {Server}=require('Socket.io')
-let io=new Server(expressServer);
-
-app.get('/',function(req,res){
-    res.sendFile(__dirname+"/index.html");
-
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 })
-io.on('connection',function(Socket){
-    console.log("new user Conncted")
+io.on('connection', function (socket) {
+    socket.on('chat', function (msg) {
+        io.emit('chat_transfer', msg)
+    })
 })
- expressServer.listen(2000,function(){
-     console.log("Server Running @ 2000");
- })
+expressServer.listen(2000, function () {
+    console.log("Server Running @ 2000");
+})
